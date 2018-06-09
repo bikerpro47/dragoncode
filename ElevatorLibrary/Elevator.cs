0b01dc50b4e24629526d784d2d814d21f55f14dd
Duane Ronan <m3style@gmail.com>
2018-06-09 18:52:12 -0500

    Initial commit to Elevator project

---
+++ ElevatorLibrary/Elevator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElevatorLibrary
+{
+    public class Elevator
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ ElevatorTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using ElevatorLibrary;
+using System;
+
+namespace ElevatorTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var el = new Elevator();
+
+            el.Name = "1st elevator";
+
+            Console.WriteLine($"{el.Name} is ready.");
+
+            Console.ReadLine();
+        }
+    }
+}
