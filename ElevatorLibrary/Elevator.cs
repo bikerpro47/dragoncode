5e48502449417a541a3a4e5859199f08b1f77f2c
Duane Ronan <m3style@gmail.com>
2018-06-11 20:18:24 -0500

    Need a Class level method (not in an object) to determine which elevator is closest

--- ElevatorLibrary/Elevator.cs
+++ ElevatorLibrary/Elevator.cs
@@ -7,22 +7,46 @@ namespace ElevatorLibrary
     public class Elevator
     {
         const int TopFloor = 10;
+        const int BottomFloor = 1;
 
         public string Name { get; set; }
-        public int FloorNo { get; set; }
+        public int CurrentFloor { get; set; }
+        public ElevatorStatus Status { get; set; }
 
     }
 
     public class FloorButton
     {
         public int FloorNo { get; set; }
-        public ElevatorDir Direction { get; set; }
-
+        public RequestDir Direction { get; set; }
     }
 
-    public enum ElevatorDir
+    public class WhichElevator
+    {
+        public void ChooseElevator(int RequestedFloor, int el1Current, int el2Current, int el3Current)
+        {
+            int el1Diff;
+            int el2Diff;
+            int el3Diff;
+
+            el1Diff = RequestedFloor - el1Current;
+        
+        }
+    }
+
+
+    public enum ElevatorStatus
+    {
+        MOVING_UP,
+        MOVING_DOWN,
+        STOPPED
+    }
+
+    // Elevator buttons on each floor
+    public enum RequestDir
     {
         UP,
-        DOWN
+        DOWN,
+        NOT_PRESSED
     }
 }
--- ElevatorTest/Program.cs
+++ ElevatorTest/Program.cs
@@ -7,21 +7,38 @@ namespace ElevatorTest
     {
         static void Main(string[] args)
         {
+            // Create 3 elevators
             var el1 = new Elevator
             {
                 Name = "Elevator No 1",
-                FloorNo = 1
+                CurrentFloor = 1
+            };
+            var el2 = new Elevator
+            {
+                Name = "Elevator No 2",
+                CurrentFloor = 5
+            };
+            var el3 = new Elevator
+            {
+                Name = "Elevator No 3",
+                CurrentFloor = 10
             };
 
-            var fb1 = new FloorButton { FloorNo = 1 };
-            var fb2 = new FloorButton { FloorNo = 2 };
+            // Create elevator buttons on some floors
+            var fb1 = new FloorButton { FloorNo = 1, Direction = RequestDir.NOT_PRESSED };
+            var fb2 = new FloorButton { FloorNo = 2, Direction = RequestDir.NOT_PRESSED };
+            var fb7 = new FloorButton { FloorNo = 7, Direction = RequestDir.NOT_PRESSED };
+            var fb9 = new FloorButton { FloorNo = 9, Direction = RequestDir.NOT_PRESSED };
 
-            // People press some buttons
-            fb1.Direction = ElevatorDir.UP;
-            fb2.Direction = ElevatorDir.DOWN;
+            // People press some buttons to summon an elevator
+            WhichElevator.
+            fb1.Direction = RequestDir.UP;
+            fb9.Direction = RequestDir.DOWN;
+            fb2.Direction = RequestDir.DOWN;
+            fb7.Direction = RequestDir.UP;
 
 
-            Console.WriteLine($"{el1.Name} is on Floor Number {el1.FloorNo}");
+            Console.WriteLine($"{el1.Name} is on Floor Number {el1.CurrentFloor}");
 
             Console.ReadLine();
         }
